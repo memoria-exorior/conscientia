1a3cf1563d311f4e08fa44dbc22230d79018e720
TempleCloud <tim.langford@gmail.com>
2017-12-03 14:28:23 +0000

    Made Facts editable.

--- src/components/Fact.vue
+++ src/components/Fact.vue
@@ -1,6 +1,7 @@
 <template>
     <div class='ui centered card'>
-        <div class='content'>
+        <!-- Display -->
+        <div class='content' v-show="!isEditing">
             <div class='header'>
             {{ fact.title }}
             </div>
@@ -11,15 +12,38 @@
             {{ fact.answer }}
             </div>
             <div class='extra content'>
-                <span class='right floated edit icon'>
+                <span class='right floated edit icon' v-on:click="showForm">
                     <i class='edit icon'></i>
                 </span>
             </div>
         </div>
-        <div class='ui bottom attached green basic button' v-show="fact.done">
+        <!-- Edit -->
+        <div class="content" v-show="isEditing">
+            <div class='ui form'>
+                <div class='field'>
+                    <label>Title</label>
+                    <input type='text' v-model="fact.title" >
+                </div>
+                <div class='field'>
+                    <label>Question</label>
+                    <input type='text' v-model="fact.question" >
+                </div>
+                <div class='field'>
+                    <label>Answer</label>
+                    <input type='text' v-model="fact.answer" >
+                </div>
+                <div class='ui two button attached buttons'>
+                <button class='ui basic blue button' v-on:click="hideForm">
+                    Close X
+                </button>
+                </div>
+            </div>
+        </div>
+        <!-- Buttons -->
+        <div class='ui bottom attached green basic button' v-show="!isEditing && fact.done">
             Done
         </div>
-        <div class='ui bottom attached red basic button' v-show="!fact.done">
+        <div class='ui bottom attached red basic button' v-show="!isEditing && !fact.done">
             Learn
         </div>
     </div>
@@ -28,7 +52,20 @@
 <script type = "text/javascript" >
 export default {
   name: 'Fact',
-  props: ['fact']
+  props: ['fact'],
+  data () {
+    return {
+      isEditing: false
+    }
+  },
+  methods: {
+    showForm () {
+      this.isEditing = true
+    },
+    hideForm () {
+      this.isEditing = false
+    }
+  }
 }
 </script>
 
